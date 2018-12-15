cf6acf609b3e992b46a65e0b1cd077928acb2ecf
thomasmichaelwallace <tom.wallace@devicepilot.com>
2018-12-15 21:42:55 +0000

    feat: advent 2.0

--- 02/src/One.hs
+++ 02/src/One.hs
@@ -1,6 +1,26 @@
 module One (
-  checksum
+    checksum
 ) where
 
+import Data.List
+
 checksum :: [String] -> Int
-checksum _ = 12
+checksum ids =
+    let twosThrees = map twosAndThrees ids
+        factor = contribs twosThrees
+    in factor
+
+twosAndThrees :: String -> (Int, Int)
+twosAndThrees id =
+    let groups = group . sort $ id
+        counts = map (\g -> length g) groups
+        twos = if any (==2) counts then 1 else 0
+        threes = if any (==3) counts then 1 else 0
+    in (twos, threes)
+
+contribs :: [(Int, Int)] -> Int
+contribs twosThrees =
+    let (twos, threes) = unzip twosThrees
+        a = sum twos
+        b = sum threes
+    in a * b
